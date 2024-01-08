e07d31f12fb2de0a29e36d3f366b43203586fdab
Morten Lied Johansen <mortenjo@ifi.uio.no>
2024-01-08 21:40:42 +0100

    Create basic layout and footer

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,30 @@
 <script>
-	import '../app.scss';
+  import '../app.scss';
 </script>
 
-<slot />
+<main class="text-center text-lg-start bg-body">
+    <div class="container py-4">
+        <h1 class="mb-3">Quotes</h1>
+        <slot/>
+    </div>
+</main>
+
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-body-tertiary text-muted">
+    <!-- Section: Disclaimer  -->
+    <section class="">
+        <div class="container text-center text-md-start mt-5 p-4">
+            <p>The quotes have varied sources, and are not necessarily attributed to the correct person, and certainly
+                not factually correct.</p>
+        </div>
+    </section>
+    <!-- Section: Disclaimer  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4 bg-body-secondary">
+        © 2024 Copyright:
+        <a class="text-reset fw-bold" href="https://home.ibidem.no/">Morten Lied Johansen</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Sequi ipsam rem aspernatur. Illo id repellendus est dolorum. Repellat autem quas voluptas aspernatur suscipit
+    necessitatibus. Nihil deserunt ut illo in ex.</p>
+<p>Sunt dolorum consequatur facilis aut officiis autem veritatis nulla. Velit quo ipsum cum. Laudantium est unde id
+    dolor. Ducimus cum unde quibusdam. Aut reiciendis neque ut reprehenderit voluptatibus libero occaecati
+    provident.</p>
+<p>Quam voluptatem sit dolorem accusamus sint dolor autem. Error asperiores dolore eos. Voluptatem autem sint saepe
+    nihil sed hic maxime. Delectus dicta velit mollitia rerum est ducimus repellendus.</p>
+<p>Et nihil quae architecto aut. Nostrum ut quo enim aut quas est fuga. Quisquam quia odit et ipsam reiciendis
+    nobis.</p>
+<p>Quo eius laboriosam quo facilis quo ut quibusdam. Recusandae aliquam saepe laudantium doloremque excepturi voluptatem
+    magnam. Dignissimos nemo provident deleniti iste repellat rerum voluptates aliquid. Iste id nihil et porro dolorem
+    repudiandae quis. Accusamus quod hic est et qui eum praesentium tempore.</p>
