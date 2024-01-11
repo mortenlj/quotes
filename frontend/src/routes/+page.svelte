2c2e1595319f1d9595e6882e134d5f946e6ac464
Morten Lied Johansen <mortenjo@ifi.uio.no>
2024-01-11 16:22:07 +0100

    Load random quote from api on front page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,12 +1,16 @@
-<p>Sequi ipsam rem aspernatur. Illo id repellendus est dolorum. Repellat autem quas voluptas aspernatur suscipit
-    necessitatibus. Nihil deserunt ut illo in ex.</p>
-<p>Sunt dolorum consequatur facilis aut officiis autem veritatis nulla. Velit quo ipsum cum. Laudantium est unde id
-    dolor. Ducimus cum unde quibusdam. Aut reiciendis neque ut reprehenderit voluptatibus libero occaecati
-    provident.</p>
-<p>Quam voluptatem sit dolorem accusamus sint dolor autem. Error asperiores dolore eos. Voluptatem autem sint saepe
-    nihil sed hic maxime. Delectus dicta velit mollitia rerum est ducimus repellendus.</p>
-<p>Et nihil quae architecto aut. Nostrum ut quo enim aut quas est fuga. Quisquam quia odit et ipsam reiciendis
-    nobis.</p>
-<p>Quo eius laboriosam quo facilis quo ut quibusdam. Recusandae aliquam saepe laudantium doloremque excepturi voluptatem
-    magnam. Dignissimos nemo provident deleniti iste repellat rerum voluptates aliquid. Iste id nihil et porro dolorem
-    repudiandae quis. Accusamus quod hic est et qui eum praesentium tempore.</p>
+<script lang="ts">
+    import type {PageData} from './$types';
+
+    export let data: PageData;
+</script>
+
+<figure>
+    <blockquote class="blockquote">
+        <p>{data.quote}</p>
+    </blockquote>
+    {#if data.dedication}
+        <figcaption class="blockquote-footer">
+            {data.dedication}
+        </figcaption>
+    {/if}
+</figure>
---
+++ frontend/src/routes/+page.ts
@@ -0,0 +1,12 @@
+import type {PageLoad} from './$types';
+
+export const ssr = false
+
+export const load: PageLoad = async ({fetch}) => {
+    const res = await fetch(`/api/v1/quotes/`);
+    const item = await res.json();
+
+    console.log(item);
+
+    return item;
+};
