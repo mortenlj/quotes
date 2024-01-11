91308cf57c37058fb41896e7abda705961d7106a
Morten Lied Johansen <mortenjo@ifi.uio.no>
2024-01-11 13:36:22 +0100

    Add links to footer

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -10,15 +10,47 @@
 </main>
 
 <!-- Footer -->
-<footer class="text-center text-lg-start bg-body-tertiary text-muted">
-    <!-- Section: Disclaimer  -->
+<footer class="text-center text-lg-start bg-body-tertiary text-muted py-3 my-4">
     <section class="">
-        <div class="container text-center text-md-start mt-5 p-4">
-            <p>The quotes have varied sources, and are not necessarily attributed to the correct person, and certainly
-                not factually correct.</p>
+        <div class="container text-center text-md-start">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <!-- Section: Disclaimer  -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="fas fa-gem me-3"></i>Disclaimer
+                    </h6>
+                    <p>
+                        The quotes have varied sources, and are not necessarily attributed to the correct person, and
+                        certainly not factually correct.
+                    </p>
+                </div>
+                <!-- Section: Disclaimer  -->
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Links
+                    </h6>
+                    <p>
+                        <a href="https://github.com/mortenlj/quotes" class="text-reset">Source</a>
+                    </p>
+                    <p>
+                        <a href="https://github.com/mortenlj/quotes/issues" class="text-reset">Issue tracker</a>
+                    </p>
+                    <p>
+                        <a href="/docs" class="text-reset">API documentation</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
         </div>
     </section>
-    <!-- Section: Disclaimer  -->
 
     <!-- Copyright -->
     <div class="text-center p-4 bg-body-secondary">
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,10 +8,19 @@ const config = {
 	preprocess: [vitePreprocess({})],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+
+		prerender: {
+			handleHttpError: ({path, referrer, message}) => {
+				// ignore links to /docs, which are handled by the backend
+				if (path === '/docs') {
+					return;
+				}
+
+				// otherwise fail the build
+				throw new Error(message);
+			}
+		},
 	}
 };
 
